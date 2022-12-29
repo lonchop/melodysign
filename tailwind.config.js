9d3532cd550913205a7cf4ea8e9413ea304c6aaa
Orangel Jose <goranjose16@gmail.com>
2022-12-29 10:25:19 -0400

    Add new feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
   content: ["./public/*.html"],
   theme: {
     extend: {
-      backgroundImage: {
-        logo: "url('./src/assets/melodysign-logo.png')",
+      colors: {
+        "background-color": "#f1e4c1",
+        "primary-color": "#0e1b3e",
+        "secondary-color": "#191919",
+        "black-color": "#0a0a0a",
+        "white-color": "#ffffff",
       },
       fontFamily: {
         quicksand: ["Quicksand", "sans-serif"],
