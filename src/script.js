25b98bd5e410ca1daba48bd8be29343da07fa2dd
Orangel Jose <goranjose16@gmail.com>
2022-12-30 16:03:00 -0400

    Add new form

---
+++ src/script.js
@@ -0,0 +1,29 @@
+console.log("hola");
+
+const $form = document.getElementById("form");
+const $buttonMailto = document.getElementById("mailto");
+
+$form.addEventListener("submit", handleSubmit);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const form = new FormData(this);
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    $form.reset();
+    alert("Gracias por contatarme te escribire pronto");
+  } else {
+    $buttonMailto.setAttribute(
+      "href",
+      `mailto:pewer18@gmail.com?subject=${form.get("name")}&body=${form.get(
+        "email"
+      )}`);
+    $buttonMailto.click();
+  }
+}
